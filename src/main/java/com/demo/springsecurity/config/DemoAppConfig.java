6843d9194acc3e00f812ed3f7899a166cee4d854
Suraj Gupta <surajrocks1997@gmail.com>
2022-08-17 20:20:19 +0530

    setting up view resolve and app config

---
+++ src/main/java/com/demo/springsecurity/config/DemoAppConfig.java
@@ -0,0 +1,23 @@
+package com.demo.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.demo.springsecurity")
+public class DemoAppConfig {
+
+//	define a bean for view resolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver();
+	}
+}
